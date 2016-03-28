e62a60bf6d8f5b654f537e3e05cd8b985b783175
Bill Wong <billmwong@gmail.com>
2016-03-28 18:47:09 -0400

    deleted deprecated file

--- localAuth.js
+++
@@ -1,6 +0,0 @@
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-
-// var User = require('./models/userModel');
-
-module.exports = passport.use(new LocalStrategy(User.authenticate()));
