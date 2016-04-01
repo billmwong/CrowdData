6e31573a7417e4a8a8e244c209dc2db99fa953cc
Bill Wong <billmwong@gmail.com>
2016-03-31 21:20:38 -0400

    formatting

--- public/js/main.js
+++ public/js/main.js
@@ -28,6 +28,7 @@ app.controller('mainController', function ($scope, $http, $location) {
   $http.get('/api/getUser')
     .success(function(data){
       if(data.user){
+        // The user is logged in
         console.log ('logged in');
         $scope.loggedIn = true;
         $http.get('/api/getSurvey')
@@ -35,26 +36,23 @@ app.controller('mainController', function ($scope, $http, $location) {
             console.log("Current survey: ");
             console.log(data.survey);
             $scope.survey = data.survey;
-            $scope.survey.questions = [{  // Short list of questions, max of 3 or 5
-      id: 1,
-      type: "mc",
-      content: "Are you?",
-      Answers: ["yeah totally", "sure"] // of strings
-    },
-    {
-      id: 2,
-      type: "mc",
-      content: "How about now?",
-      Answers: ["nope", "maybe a little"] // of strings
-    }
-  ];
+            $scope.survey.questions = [
+              {  // Short list of questions, max of 3 or 5
+                id: 1,
+                type: "mc",
+                content: "Are you?",
+                Answers: ["yeah totally", "sure"] // of strings
+              },
+              {
+                id: 2,
+                type: "mc",
+                content: "How about now?",
+                Answers: ["nope", "maybe a little"] // of strings
+              }
+            ];
             $scope.questions = data.survey.questions;
           })
           .error(handleError);
-      }else{
-        console.log('not logged in');
-        console.log(data.user);
-        // $location.path('/login');
       }
     });
 
