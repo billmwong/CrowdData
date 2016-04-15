5a2fad75cce4f393a185613d3d8b8dfc2b666243
Charlie Mouton <charlie@moutons.org>
2016-04-15 10:12:51 -0400

    debugging  variables and fixed main controller declaration error.

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 var app = angular.module('crowddata', ['ngRoute'])
-  .run(function($rootScope){
+  .run(function ($rootScope) {
+    $rootScope.Setup = {};
     $rootScope.Setup.numOfQuestions = 0;
     $rootScope.questionNumber = 1;
   });
@@ -114,7 +115,7 @@ app.controller('newSurveyController', function ($scope, $rootScope, $http, $loca
 
   $scope.qProgress = function () {
     console.log('Setup.numOfQuestions' + $rootScope.Setup.numOfQuestions);
-    if ($rootScope.Setup.numOfQuestions > $rootScope.questionNumber){
+    if ($rootScope.Setup.numOfQuestions > $rootScope.questionNumber) {
       $rootScope.questionNumber += 1;
       $location.path('/newsurvey/creating_qs');
       console.log('qProgress just did its thing');
@@ -123,7 +124,7 @@ app.controller('newSurveyController', function ($scope, $rootScope, $http, $loca
     }
   };
 
-  $scope.addOption = function(){
+  $scope.addOption = function () {
     numOfOptions += 1;
     $('#addOptionBtn').before("<div class='form-group label-floating'><br><label class='control-label' for='surveyTitle'>Option " + Setup.numOfOptions + "</label><input class='form-control' id='surveyTitle' type='text' ng-focus='typingTitle=true' ng-blur='typingTitle=false' ng-model='q.{{questionNumber}}.response." + Setup.numOfOptions + "'><small class='text-muted' ng-show='typingTitle'> </small></div>");
 
